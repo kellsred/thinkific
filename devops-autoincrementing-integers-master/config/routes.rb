1188011a2d5d40aa8ece8493c93b428d7ee5b991
Paul Smith <send2paulsmith@gmail.com>
2021-10-24 22:00:48 -0400

    initial save of early working version

---
+++ devops-autoincrementing-integers-master/config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  post 'users/create'
+  get 'users/create', to: 'users#create'
+  namespace :api do
+    namespace :v1 do
+      get 'integer', to: 'user_integers#show'
+      put 'integer/increment', to: 'user_integers#increment'
+      put 'integer', to: 'user_integers#update'
+    end
+  end
+end
